33ee781ebde1d3bb815d17af4f1913555482f280
Bogdan <53011638+bodiabeliy@users.noreply.github.com>
2021-06-10 00:14:02 +0300

    fixing: adaptive, list (hide/show) button-text

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ function App() {
   const ShowDialogList = () => {
     setOpen(true);
   };
-//   const HideDialogList = () => {
-//    setOpen(false);
-//  };
+  const HideDialogList = () => {
+   setOpen(false);
+ };
 
  // list array
     // noteType - is type of write notes 
@@ -40,28 +40,60 @@ function App() {
     text: 'Новый комментарий от Костянтин Костянтинопольський Minutes Book Analysis',
     timeCondition: '14:23 15 April',
     noteType:3
+  },  {
+    statusIcon: './images/ok.svg',
+    text: 'Shareholder’s, Optionee’s & Investor’s Documents Analysis изменен статус на',
+    timeCondition: '15:19 21 April',
+    statusCompled: 'Done',
+    noteType:2
+  },
+  {
+    statusIcon: './images/toRightArrow.svg',
+    text: 'New task request from Костянтин Костянтинопольський', 
+    timeCondition: '14:23 18 April',
+    noteType:1
+  },
+   {
+    statusIcon: './images/chat.svg',
+    text: 'Новый комментарий от Костянтин Костянтинопольський Minutes Book Analysis',
+    timeCondition: '14:23 15 April',
+    noteType:3
   }
   
  ]
 
   return (
-    <div className="App">
+    <div 
+      className="App"
+      style={{
+        display: window.innerWidth >= 600? 'flex' :'block'
+      }}
+    >
+      {console.log(window.innerWidth)}
       <div className="list-contant__wrapper">
-        <div className="list-contant__show-btn">
+        <div 
+          className="list-contant__show-btn"
+        >
+        {open === true ?
+          <Fragment>
+            <button
+            className='show-btn__bell'
+            onClick={HideDialogList}
+            >
+            <img src="./images/close.svg" alt="" />
+          </button>
+          <ListArea notes={listOfEvents}/>
+          </Fragment>
+          
+          :
           <button
-           className='show-btn__bell'
-           onClick={ShowDialogList}
-           >
+          className='show-btn__bell'
+          onClick={ShowDialogList}
+          >
             <img src="./images/bell.svg" alt="" />
           </button>
-        </div>
-        {open === false ?
-          <p>Click to bell for open dialog list...</p>
-          :
-          <Fragment>
-            <ListArea notes={listOfEvents}/>
-          </Fragment>
         }
+        </div>
       </div>
       <LevelEnglish />
     </div>
--- src/components/ListArea.js
+++ src/components/ListArea.js
@@ -1,10 +1,27 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import ListEvent from './ListEvent'
+
+
 function ListArea(props) {
+  const [notifications, setNotifications] = useState(false);
+
+  const AllNotifications = () => {
+    setNotifications(true);
+  };
+  const HideNotifications = () => {
+    setNotifications(false);
+ };
+
  return (
   <Fragment>
     <div className="list-contant__list-area">
-     <div className="list-area__wrapper">
+     <div 
+      className="list-area__wrapper"
+      style={{
+        height: notifications === false? '100%':'300px',
+        overflowY: notifications === false? 'auto': 'none'
+      }}
+      >
      {props.notes.map(note => {
      return (
       <ListEvent notes_detailes={note}/>
@@ -12,9 +29,15 @@ function ListArea(props) {
      })}
      </div>
      <div className="list-area__text-btn">
-      <h3>
-      ALL NOTIFICATIONS
-      </h3>
+     {notifications === true?
+        <h3 onClick={HideNotifications}>
+          ALL NOTIFICATIONS
+         </h3>
+       :
+       <h3 onClick={AllNotifications}>
+          HIDE NOTIFICATIONS
+       </h3>
+     }
      </div>
     </div>
   </Fragment>
